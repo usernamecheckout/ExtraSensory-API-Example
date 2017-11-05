432a587883349c94ea5822b0343b0fafa314c116
UsernameChecksOut <UsernameChecksOut@Williams-MacBook-Pro.local>
2017-11-05 12:17:52 -0800

    added the page flow

---
+++ app/src/main/java/edu/ucsd/calab/usingextrasensorylabels/graphActivity.java
@@ -0,0 +1,29 @@
+package edu.ucsd.calab.usingextrasensorylabels;
+
+import android.content.IntentFilter;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+
+/**
+ * Created by UsernameChecksOut on 11/5/17.
+ */
+
+public class graphActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main_page);
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+    }
+
+    @Override
+    public void onPause() {
+        super.onPause();
+    }
+}
